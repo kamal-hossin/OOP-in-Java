deae85421f6630fd90d6f64c8b48adb3862bd12d
kamal-hossin <kamal.hossin.cse@gmail.com>
2026-06-07 08:53:30 +0600

    Using super class accsess parent class

---
+++ supur_class.java
@@ -0,0 +1,20 @@
+class Animal {
+  public void animalSound() {
+    System.out.println("The animal makes a sound");
+  }
+}
+
+class Dog extends Animal {
+  public void animalSound() {
+    super.animalSound(); // Call the parent method
+    System.out.println("The dog says: bow wow");
+  }
+}
+
+public class supur_class {
+  public static void main(String[] args) {
+    Dog myDog = new Dog();
+    myDog.animalSound();
+  }
+}
+
